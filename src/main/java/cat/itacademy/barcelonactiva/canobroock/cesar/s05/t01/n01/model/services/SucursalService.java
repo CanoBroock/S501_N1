bed343010a02d28753ae7eb412fd9d3a22e001e2
CanoBroock <canobroock@gmail.com>
2022-11-30 10:54:12 +0100

    correcion

--- src/main/java/cat/itacademy/barcelonactiva/canobroock/cesar/s05/t01/n01/model/services/SucursalService.java
+++ src/main/java/cat/itacademy/barcelonactiva/canobroock/cesar/s05/t01/n01/model/services/SucursalService.java
@@ -1,11 +1,12 @@
 package cat.itacademy.barcelonactiva.canobroock.cesar.s05.t01.n01.model.services;
 
+import cat.itacademy.barcelonactiva.canobroock.cesar.s05.t01.n01.model.DTO.SucursalDTO;
 import cat.itacademy.barcelonactiva.canobroock.cesar.s05.t01.n01.model.domain.Sucursal;
 
 import java.util.List;
 
 public interface SucursalService {
-    public List<Sucursal> getAllSucursales();
+    public List<SucursalDTO> getAllSucursales();
     public void saveSucursal(Sucursal sucursal);
     public Sucursal getSucursalById(long id);
     public void deleteSucursalById(long id);
--- src/main/java/cat/itacademy/barcelonactiva/canobroock/cesar/s05/t01/n01/model/services/SucursalServiceImpl.java
+++ src/main/java/cat/itacademy/barcelonactiva/canobroock/cesar/s05/t01/n01/model/services/SucursalServiceImpl.java
@@ -1,5 +1,6 @@
 package cat.itacademy.barcelonactiva.canobroock.cesar.s05.t01.n01.model.services;
 
+import cat.itacademy.barcelonactiva.canobroock.cesar.s05.t01.n01.model.DTO.SucursalDTO;
 import cat.itacademy.barcelonactiva.canobroock.cesar.s05.t01.n01.model.domain.Sucursal;
 import cat.itacademy.barcelonactiva.canobroock.cesar.s05.t01.n01.model.repository.SucursalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class SucursalServiceImpl implements SucursalService {
@@ -14,9 +16,17 @@ public class SucursalServiceImpl implements SucursalService {
     @Autowired
     private SucursalRepository sucursalRepository;
 
+    private SucursalDTO convertEntityToDTO(Sucursal sucursal) {
+        SucursalDTO sucursalDTO = new SucursalDTO();
+        sucursalDTO.setPk_SucursalID((int) sucursal.getPk_SucursalID());
+        sucursalDTO.setNomSucursal(sucursal.getNomSucursal());
+        sucursalDTO.setPaisSucursal(sucursal.getPaisSucursal());
+        return sucursalDTO;
+    }
+
     @Override
-    public List<Sucursal> getAllSucursales() {
-        return sucursalRepository.findAll();
+    public List<SucursalDTO> getAllSucursales() {
+        return sucursalRepository.findAll().stream().map(this::convertEntityToDTO).collect(Collectors.toList());
     }
 
     @Override
